92b2cab98143cea08920a662a855104a45f7ce19
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-02-12 17:41:00 +0100

    feat: test for single private card

---
+++ src/components/PrivateFilmCard/PrivateFilmCard.test.tsx
@@ -0,0 +1,64 @@
+import { fireEvent, render } from '@testing-library/react';
+import { useDeleteFilmByIdMutation } from '../../services/film/api';
+import PrivateFilmCard from '.';
+
+jest.mock('../../services/film/api');
+const mockedUseDeleteFilmByIdMutation = jest.mocked(useDeleteFilmByIdMutation);
+
+const privateFilmCardProps = {
+  film: {
+    title: 'Avengers',
+    filmId: '123',
+    coverImg: 'cover',
+    plot: 'plot',
+    nationOfProduction: 'usa',
+    rating: 0.2,
+    funFacts: 'none',
+    filmGenre: [],
+  },
+  handleFilmFormVisibility: jest.fn(),
+};
+const renderComponent = () => {
+  return render(
+    <PrivateFilmCard
+      film={privateFilmCardProps.film}
+      handleFilmFormVisibility={privateFilmCardProps.handleFilmFormVisibility}
+    />
+  );
+};
+
+beforeEach(() => {
+  mockedUseDeleteFilmByIdMutation.mockReturnValue([
+    jest.fn(),
+    { loading: false, errore: null, reset: jest.fn() },
+  ]);
+});
+
+describe('PrivateFilmCard', () => {
+  test('renders film send by prop', () => {
+    const { getByText, getByAltText } = renderComponent();
+
+    expect(getByText('Avengers')).toBeInTheDocument();
+    expect(getByText('plot')).toBeInTheDocument();
+    expect(getByText('0.2 usa none')).toBeInTheDocument();
+
+    expect(getByAltText('...')).toHaveAttribute('src', 'cover');
+  });
+
+  test('testing edit function', () => {
+    const { getByText } = renderComponent();
+
+    fireEvent.click(getByText('Edit'));
+
+    expect(privateFilmCardProps.handleFilmFormVisibility).toHaveBeenCalledWith(
+      privateFilmCardProps.film
+    );
+  });
+  test('testing delete function', () => {
+    const { getByText } = renderComponent();
+
+    fireEvent.click(getByText('Delete'));
+
+    expect(mockedUseDeleteFilmByIdMutation).toHaveBeenCalled();
+  });
+});
