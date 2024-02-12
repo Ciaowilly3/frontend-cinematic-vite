e8081f40cc955419ebeb4606c2331d5884739650
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-02-12 17:40:51 +0100

    feat: test for single link

--- src/components/PrimaryLink/PrimaryLink.test.tsx
+++ src/components/PrimaryLink/PrimaryLink.test.tsx
@@ -1,19 +1,26 @@
-import { render, screen } from "@testing-library/react";
-import PrimaryLink from ".";
-import { MemoryRouter } from "react-router-dom";
-import { PathsEnum } from "../../enums/PathsEnum";
+import { fireEvent, render, screen } from '@testing-library/react';
+import PrimaryLink from '.';
+import { PathsEnum } from '../../enums/PathsEnum';
+import { MemoryRouter } from 'react-router';
 
 const renderPrimaryLink = (path: Partial<Location>) => {
   render(
     <MemoryRouter initialEntries={[path]}>
-      <PrimaryLink path={PathsEnum.ABOUT_US} style={[]} content={"link"} />
+      <PrimaryLink path={PathsEnum.ABOUT_US} style={[]} content={'link'} />
     </MemoryRouter>
   );
 };
 
-describe("tests for primaryLinks", () => {
+describe('tests for primaryLinks', () => {
   test('expects to find the content prop link" ', () => {
     renderPrimaryLink({ pathname: PathsEnum.ABOUT_US });
-    expect(screen.getByText("Link")).toBeInTheDocument();
+    expect(screen.getByText('Link')).toBeInTheDocument();
+  });
+  test('expects that onClick link redirects to the path page', () => {
+    renderPrimaryLink({ pathname: PathsEnum.ABOUT_US });
+
+    fireEvent.click(screen.getByText('Link'));
+
+    expect(location.pathname).toBe(PathsEnum.ABOUT_US);
   });
 });
