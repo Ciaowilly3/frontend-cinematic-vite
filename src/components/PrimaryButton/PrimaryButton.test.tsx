8325255010804ba551d42b87f8965609d6425c46
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-02-12 17:40:44 +0100

    feat: test for single button

--- src/components/PrimaryButton/PrimaryButton.test.tsx
+++ src/components/PrimaryButton/PrimaryButton.test.tsx
@@ -1,27 +1,27 @@
-import { render, fireEvent } from "@testing-library/react";
-import PrimaryButton from ".";
-import { BtnStyles } from "./PrimaryButton";
+import { render, fireEvent } from '@testing-library/react';
+import PrimaryButton from '.';
+import { BtnStyles } from './PrimaryButton';
 
-const style: BtnStyles = ["btnDanger"];
+const style: BtnStyles = ['btnDanger'];
 
 const mockedOnClick = jest.fn();
 
 const btnProps = {
   onClickFunction: mockedOnClick,
   icon: undefined,
-  content: "contenuto",
+  content: 'contenuto',
   style: style,
   disable: false,
 };
-describe("btn primary test", () => {
-  test("btn should be defined", () => {
+describe('PrimaryButton', () => {
+  test('Button should be defined', () => {
     const { getByText } = render(<PrimaryButton {...btnProps} />);
-    const btn = getByText("contenuto");
+    const btn = getByText('contenuto');
     expect(btn).toBeDefined();
   });
-  test("btn should call the function when pressed", () => {
+  test('Button should call the function when pressed', () => {
     const { getByText } = render(<PrimaryButton {...btnProps} />);
-    fireEvent.click(getByText("contenuto"));
+    fireEvent.click(getByText('contenuto'));
     expect(mockedOnClick).toHaveBeenCalledTimes(1);
   });
 });
