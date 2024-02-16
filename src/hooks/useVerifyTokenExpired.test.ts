5a34877e1d134f7d560d26cff695a28f50c4d3aa
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-02-16 14:24:12 +0100

    refactor: refactored test e added navigate in custom hook

--- src/hooks/useVerifyTokenExpired.test.ts
+++ src/hooks/useVerifyTokenExpired.test.ts
@@ -1,19 +1,28 @@
 import { renderHook } from '@testing-library/react';
 import { useDispatch, useSelector } from 'react-redux';
 import useVerifyTokenExpired from './useVerifyTokenExpired';
+import { deleteWebToken } from '../slices/auth/authTokenSlice';
+
+import { useNavigate } from 'react-router';
 
 jest.mock('../store/store');
+jest.mock('../slices/auth/authTokenSlice');
 jest.mock('react-redux');
+jest.mock('react-router');
 const mockedUseDispatch = jest.mocked(useDispatch);
 const mockedUseSelector = jest.mocked(useSelector);
+const mockedUseNavigate = jest.mocked(useNavigate);
+const mockedDeleteWebToken = jest.mocked(deleteWebToken);
 
 const mockedDispatch = jest.fn();
+const mockedNavigate = jest.fn();
 
 const hookRender = () => renderHook(() => useVerifyTokenExpired());
 
 describe('useVerifyTokenExpired', () => {
   beforeEach(() => {
     mockedUseDispatch.mockReturnValue(mockedDispatch);
+    mockedUseNavigate.mockReturnValue(mockedNavigate);
   });
   test('verify that token is not expired and perxists in store', () => {
     const mockedToken = {
@@ -25,6 +34,8 @@ describe('useVerifyTokenExpired', () => {
     result.current.verifyTokenExpired();
 
     expect(mockedDispatch).not.toHaveBeenCalled();
+    expect(mockedDeleteWebToken).not.toHaveBeenCalled();
+    expect(mockedNavigate).not.toHaveBeenCalled();
   });
   test('verify that token is  expired and not perxists in store', () => {
     const mockedToken = {
@@ -37,5 +48,7 @@ describe('useVerifyTokenExpired', () => {
     result.current.verifyTokenExpired();
 
     expect(mockedDispatch).toHaveBeenCalled();
+    expect(mockedDeleteWebToken).toHaveBeenCalled();
+    expect(mockedNavigate).toHaveBeenCalled();
   });
 });
--- src/hooks/useVerifyTokenExpired.ts
+++ src/hooks/useVerifyTokenExpired.ts
@@ -1,17 +1,22 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { authTokenState, deleteWebToken } from '../slices/auth/authTokenSlice';
 import { useCallback } from 'react';
+import { useNavigate } from 'react-router';
 
 const useVerifyTokenExpired = () => {
   const dispatch = useDispatch();
-  const authToken = useSelector((state: authTokenState) => state);
+  const navigate = useNavigate();
+  const authToken = useSelector(
+    (state: { authToken: authTokenState }) => state.authToken
+  );
 
   const verifyTokenExpired = useCallback(() => {
     const { expirationDate } = authToken;
-    console.log(authToken.expirationDate + ' e invece ora ' + Date.now());
+    console.log(authToken + ' e invece ora ' + Date.now());
     if (expirationDate && parseInt(expirationDate) < Date.now()) {
       localStorage.removeItem('persist:authToken');
       dispatch(deleteWebToken());
+      navigate('/');
     }
   }, [dispatch]);
 
