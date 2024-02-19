b7add4086ef230c76b68f7ceb132e15542bb131b
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-02-19 17:47:09 +0100

    feat: test completed

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,10 +1,38 @@
-import Header from "./Header";
-import { render } from "@testing-library/react";
+import { MemoryRouter } from 'react-router';
+import Header from '.';
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import { toggleFormModal } from '../../slices/auth/formModalSlice';
 
-describe("Header", () => {
-  test("renders logo", () => {
-    const { getByTestId } = render(<Header />);
-    const pippo = getByTestId("header");
-    expect(pippo).toBeDefined();
+const mockedDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockedDispatch,
+}));
+jest.mock('../../slices/auth/formModalSlice', () => ({
+  toggleFormModal: jest.fn(),
+}));
+const renderComponent = () =>
+  render(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
+  );
+describe('Header', () => {
+  test('renders component correctly', () => {
+    const { getByTestId, getByText, container } = renderComponent();
+    expect(getByTestId('header')).toBeInTheDocument();
+    expect(getByText('About Us')).toBeInTheDocument();
+    expect(container.querySelector('.btn-flush')).toBeInTheDocument();
+  });
+  test('dispatch is call on button click', async () => {
+    const { container } = renderComponent();
+    const btn = container.querySelector('.btn-flush');
+
+    btn && fireEvent.click(btn);
+
+    await waitFor(() => {
+      expect(mockedDispatch).toHaveBeenCalled();
+      expect(toggleFormModal).toHaveBeenCalled();
+    });
   });
 });
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
-import { FaSearch, FaUser } from "react-icons/fa";
-import cinematicLogo from "/cinematic-logo.png";
-import "./Header.css";
-import { Link } from "react-router-dom";
-import { PathsEnum } from "../../enums/PathsEnum";
-import { useDispatch } from "react-redux";
-import { toggleFormModal } from "../../slices/auth/formModalSlice";
-import PrimaryLink from "../PrimaryLink";
+import { FaUser } from 'react-icons/fa';
+import cinematicLogo from '/cinematic-logo.png';
+import './Header.css';
+import { Link } from 'react-router-dom';
+import { PathsEnum } from '../../enums/PathsEnum';
+import { useDispatch } from 'react-redux';
+import { toggleFormModal } from '../../slices/auth/formModalSlice';
+import PrimaryLink from '../PrimaryLink';
+import { PrimaryButton } from '..';
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -16,33 +17,26 @@ const Header = () => {
           <div className="nav-container text-white d-flex justify-content-center align-items-center h-100 gap-3">
             <PrimaryLink
               path={PathsEnum.ABOUT_US}
-              style={["txtMyThird", "linkFlush"]}
+              style={['txtMyThird', 'linkFlush']}
               content="About Us"
             />
           </div>
         </div>
         <div className="col-4">
           <div className="logo-container w-100">
-            <Link to={"/"}>
+            <Link to={'/'}>
               <img src={cinematicLogo} alt="logo" />
             </Link>
           </div>
         </div>
         <div className="col-4">
           <div className="icon-container text-white d-flex justify-content-center align-items-center h-100 gap-3 ">
-            <button
+            <PrimaryButton
               type="button"
-              className="btn-flush btn-third text-my-secondary"
-              onClick={() => dispatch(toggleFormModal())}
-            >
-              <FaUser />
-            </button>
-            <button
-              type="button"
-              className="btn-flush btn-third text-my-secondary"
-            >
-              <FaSearch />
-            </button>
+              style={['txtMySecondary', 'btnFlush']}
+              icon={FaUser}
+              onClickFunction={() => dispatch(toggleFormModal())}
+            />
           </div>
         </div>
       </div>
