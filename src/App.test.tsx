7afb929acf22aae8713cd2c34ad545a6c283c312
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-02-19 17:46:35 +0100

    feat: begin of test

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import LoginFormModal from './components/LoginFormModal';
+import App from './App';
+import { render } from '@testing-library/react';
+
+const renderComponent = () => render(<App />);
+
+describe('App', () => {
+  test('');
+});
