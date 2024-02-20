5fe3d185ce75ca47d1c924c8075b5fc6505b081a
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-02-20 12:34:59 +0100

    refactor: refactored style mock file

--- mocks/styleMock.js
+++ mocks/styleMock.js
@@ -1,8 +1,1 @@
-const PublicFilmCardStyle = require('../src/components/PublicFilmCard/PublicFilmCardStyle');
-console.log('PublicFilmCardStyle:', PublicFilmCardStyle);
-console.log('variable: ', process.env.USE_REAL_STYLES);
-
-module.exports = {
-  ...(!process.env.USE_REAL_STYLES && { mockStyles: true }),
-  ...(process.env.USE_REAL_STYLES && PublicFilmCardStyle),
-};
+module.exports = {};
