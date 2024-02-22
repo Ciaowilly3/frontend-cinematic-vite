a762a2f5a152dde95c9eaba68061f96e28a6ae93
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-02-22 15:38:43 +0100

    refactor: refactored test

--- src/components/PrimaryLink/PrimaryLink.test.tsx
+++ src/components/PrimaryLink/PrimaryLink.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import PrimaryLink from '.';
 import { PathsEnum } from '../../enums/PathsEnum';
 import { MemoryRouter } from 'react-router';
@@ -15,14 +15,5 @@ describe('tests for primaryLinks', () => {
   test('expects to find the content prop link" ', () => {
     renderPrimaryLink({ pathname: PathsEnum.ABOUT_US });
     expect(screen.getByText('Link')).toBeInTheDocument();
-  });
-  test('expects that onClick link redirects to the path page', async () => {
-    const { getByText } = renderPrimaryLink({ pathname: PathsEnum.ABOUT_US });
-
-    fireEvent.click(getByText('Link'));
-
-    await waitFor(() => {
-      expect(location.pathname).toBe(PathsEnum.ABOUT_US);
-    });
   });
 });
