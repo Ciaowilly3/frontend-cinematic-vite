214d3378179e30a4fbde5fe75d6dacfbefffaf20
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-02-22 15:37:53 +0100

    feat: implemented test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
-import LoginFormModal from './components/LoginFormModal';
 import App from './App';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
+
+const mockedIsShownLoginForm = jest.fn().mockReturnValue(false);
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useSelector: () => mockedIsShownLoginForm(),
+}));
+
+jest.mock('./hooks/useVerifyTokenExpired', () => ({
+  __esModule: true,
+  default: jest.fn().mockReturnValue({ verifyTokenExpired: jest.fn() }),
+}));
+
+jest.mock('./components/LoginFormModal', () =>
+  jest.fn().mockReturnValue(<div>Login form</div>)
+);
 
 const renderComponent = () => render(<App />);
 
 describe('App', () => {
-  test('');
+  test('App component renders form correctly', async () => {
+    mockedIsShownLoginForm.mockReturnValueOnce(true);
+    const { getByText } = renderComponent();
+
+    await waitFor(() => expect(getByText('Login form')).toBeInTheDocument());
+  });
 });
