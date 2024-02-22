25ec4e4ee8554def7c1a68f4ebe77583d5357d80
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-02-22 15:45:15 +0100

    feat: implemented test

--- src/pages/PrivateFilmsPage/PrivateFilmsPage.test.tsx
+++ src/pages/PrivateFilmsPage/PrivateFilmsPage.test.tsx
@@ -1,29 +1,71 @@
-import { render, waitFor } from '@testing-library/react';
+import { fireEvent, render, waitFor } from '@testing-library/react';
 import PrivateFilmPage from './PrivateFilmsPage';
-import { useRetrieveAllFilmsQuery } from '../../services/film/api';
 
-jest.mock('../../components/GenreInput');
-jest.mock('../../services/film/api');
+const mockedData = [
+  {
+    title: 'Avengers',
+    filmId: '123',
+    coverImg: 'cover',
+    plot: 'plot',
+    nationOfProduction: 'usa',
+    rating: 0.2,
+    funFacts: 'none',
+    filmGenre: [],
+  },
+];
 
-const mockedUseRetrieveAllFilmsQuery = jest.mocked(useRetrieveAllFilmsQuery);
+const mockedIsError = jest.fn().mockReturnValue(false);
+const mockedIsFetching = jest.fn().mockReturnValue(false);
+jest.mock('../../components/GenreInput', () =>
+  jest.fn().mockReturnValue(<div>Genre</div>)
+);
+jest.mock('../../components/PrivateFilmCard', () =>
+  jest.fn().mockReturnValue(<div>Avengers</div>)
+);
+jest.mock('../../components/FilmForm', () =>
+  jest.fn().mockReturnValue(<div>Form</div>)
+);
+jest.mock('../../services/film/api', () => ({
+  useRetrieveAllFilmsQuery: () => ({
+    data: mockedData,
+    isError: mockedIsError(),
+    isFetching: mockedIsFetching(),
+  }),
+}));
 
+// const mockedHandleFilmFormVisibility = jest.fn();
 const renderComponent = () => render(<PrivateFilmPage />);
 
 describe('PrivateFilmPage', () => {
   test('should render', () => {
-    mockedUseRetrieveAllFilmsQuery.mockReturnValue({} as never);
     const view = renderComponent();
 
     expect(view).toMatchSnapshot();
   });
   test('should display error when query fails', async () => {
-    mockedUseRetrieveAllFilmsQuery.mockReturnValue({
-      isError: true,
-    } as never);
+    mockedIsError.mockReturnValueOnce(true);
     const { getByText } = renderComponent();
 
     await waitFor(() => {
       expect(getByText('ERROR')).toBeInTheDocument();
+    });
+  });
+  test('should render loader when is fetching', async () => {
+    mockedIsFetching.mockReturnValueOnce(true);
+    const { getByTestId } = renderComponent();
+
+    await waitFor(() => {
+      expect(getByTestId('loader')).toBeInTheDocument();
+    });
+  });
+  test('should render filmForm when btn is clicked', async () => {
+    const { getByText } = renderComponent();
+
+    fireEvent.click(getByText('Create a film'));
+
+    await waitFor(() => {
+      expect(getByText('Form')).toBeInTheDocument();
+      // expect(mockedHandleFilmFormVisibility).toHaveBeenCalled();
     });
   });
 });
