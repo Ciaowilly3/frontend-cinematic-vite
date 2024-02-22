f79ca67447ad6760a80c6ec94046c4c980c9629b
massimiliano.muscolino <massimiliano.muscolino@aitho.it>
2024-02-22 15:46:21 +0100

    refactor: refactored api

--- src/services/film/api.ts
+++ src/services/film/api.ts
@@ -1,26 +1,24 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { BASE_URL } from "../utils";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { BASE_URL } from '../utils';
 import {
   deleteFilmById,
   makeNewFilm,
   retrieveAllFilms,
   updateFilmById,
-} from "./endpoints";
-import { authTokenState } from "../../slices/auth/authTokenSlice";
-import { RootState } from "../../store/store";
+} from './endpoints';
+import { authTokenState } from '../../slices/auth/authTokenSlice';
+import { RootState } from '../../store/store';
 
 export const filmApi = createApi({
-  tagTypes: ["films"],
-  reducerPath: "filmsApi",
+  tagTypes: ['films'],
+  reducerPath: 'filmsApi',
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
     prepareHeaders: (headers, { getState }) => {
       const authToken: authTokenState = (getState() as RootState).authToken;
-      console.log(authToken);
-
       authToken.token
-        ? headers.set("authorization", `Bearer ${authToken.token}`)
-        : "";
+        ? headers.set('authorization', `Bearer ${authToken.token}`)
+        : '';
       return headers;
     },
   }),
